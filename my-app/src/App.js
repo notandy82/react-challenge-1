2b1fc9954b0177731d0972059538a4f64b64dd30
notandy82 <notandystanek@gmail.com>
2022-10-07 10:53:03 +0000

    Add conditional form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
+import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarSimple />
+      <NavBarForm />
       <Sidebar />
     </div>
   );
