93714596ed3e9781cc828b770757977495feef02
notandy82 <notandystanek@gmail.com>
2022-10-13 10:39:29 +0000

    Add loader

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
-import PostItem from "./PostItem"
+import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
-      super(props)    
+        super(props)
+        this.state = {
+            isLoaded: false
+        }
     }
-  render() {
-    return (
-      <div className={css.Content}>
-        <div className={css.TitleBar}>
-            <h1>My Photos</h1>
-        </div>
-        <div className={css.SearchResults}>
 
-                    {/* Part 1: Creating the map function */}
-
-                        {/* {
-                    savedPosts.map((post)=>{
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random"/>
-                                <p>{post.description}</p>
-                                </div>
-                        })
-                    } */}
-
-
-                    {/* Part 2: Creating a child component */}
-
-                    <PostItem savedPosts={savedPosts} />
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
                 </div>
-      </div>
-    )
-  }
+
+                <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Content
