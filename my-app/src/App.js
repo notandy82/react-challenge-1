818845ca4ea5460b264a4862f37f26a5966e94f4
notandy82 <notandystanek@gmail.com>
2022-10-20 14:54:13 +0000

    Add hooks to contentapi version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import css from './App.module.css';
 import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
-import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
-      <ContentAPI />
+      <ContentAPIHooks />
     </div>
   );
 }
